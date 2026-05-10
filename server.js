b7e67a1cba127d2d0f49a82d25ba0393a9b878b1
Marjesmmmj <marjesmmmj@gmail.com>
2026-05-10 00:45:34 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+
+  console.log("Usuario conectado:", socket.id);
+
+  // 🔥 unirse a canal
+  socket.on("join", (room) => {
+
+    socket.join(room);
+
+    console.log(`${socket.id} se unió a ${room}`);
+
+  });
+
+  // 🔥 AUDIO
+  socket.on("audio", (data) => {
+
+    // enviar audio a todos menos al emisor
+    socket.broadcast.emit("audio", data);
+
+  });
+
+  // 🔥 desconexión
+  socket.on("disconnect", () => {
+
+    console.log("Usuario desconectado:", socket.id);
+
+  });
+
+  socket.on("ping_server", () => {
+
+  console.log("PING");
+
+  socket.emit("pong");
+
+});
+});
+
+
+server.listen(3000, () => {
+
+  console.log("Servidor corriendo en puerto 3000");
+
+});
